3b194f7fbab2f1fecb844cbef145a72743534de4
John124code <miracleolawoye7@gmail.com>
2025-05-20 14:28:23 +0100

    files for my data structure project

---
+++ proj1/index.js
@@ -0,0 +1,26 @@
+// Solution for Problem 1
+
+function sumOfDistinctElements(set1, set2) {
+    let sum = 0;
+
+    // Add elements from set1 not in set2
+    for (let i = 0; i < set1.length; i++) {
+        if (!set2.includes(set1[i])) {
+            sum += set1[i];
+        }
+    }
+
+    // Add elements from set2 not in set1
+    for (let j = 0; j < set2.length; j++) {
+        if (!set1.includes(set2[j])) {
+            sum += set2[j];
+        }
+    }
+
+    return sum;
+}
+
+// Example usage:
+const set1 = [3, 1, 7, 9];
+const set2 = [2, 4, 1, 9, 3];
+console.log(sumOfDistinctElements(set1, set2)); 
